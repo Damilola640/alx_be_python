e14847b1ad356b415a9c56adb899ab3b4bb0d0f7
Damilola640 <olatoyeclement531@gmail.com>
2025-05-23 10:38:12 +0100

    A calculator for Simple interest was added

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# Values given
+principal = 1000
+rate = 0.05
+time = 3
+
+# Here is the formula for the calculation.
+Interest = principal * rate * time
+
+# printing the values given in words
+print(f"For principal = {principal}, Rate = {rate}, and Time = {time},")
+# The final answer
+print(f"The simple interest is {float(Interest)}")
